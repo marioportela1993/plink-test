3b1f50e2c18b933ef80484b527a9adbb4605d0bd
Mario Andres Portela Gaviria <mario.portela@wolox.co>
2019-09-09 16:02:27 -0500

    middleware for authetication

--- app/constants/errors.js
+++ app/constants/errors.js
@@ -7,3 +7,6 @@ exports.REQUIRED_PASSWORD_ERROR = 'Password is required';
 exports.INVALID_PASSWORD_LENGTH_ERROR = 'Password should have at least 8 characters';
 exports.REQUIRED_PREFERRED_CURRENCY = 'Currency is required';
 exports.INVALID_PREFERRED_CURRENCY = 'Invalid currency. Enter one of the following: COP, USD, EUR.';
+exports.AUTHENTICATION_ERROR_MSG = 'User no authenticated';
+exports.INVALID_TOKEN_ERROR_MSG = 'Invalid token';
+exports.TOKEN_EXPIRED_ERROR_MSG = 'Token expired';
---
+++ app/middlewares/authentication.js
@@ -0,0 +1,29 @@
+const { authenticationError } = require('../errors');
+const logger = require('../logger');
+const { header_name } = require('../../config').common.session;
+const {
+  AUTHENTICATION_ERROR_MSG,
+  INVALID_TOKEN_ERROR_MSG,
+  TOKEN_EXPIRED_ERROR_MSG
+} = require('../constants/errors');
+const { validateToken } = require('../helpers/token');
+const { findUserByUsername } = require('../services/user');
+
+exports.authenticate = (req, _, next) => {
+  const token = req.headers[header_name];
+  if (!token) return next(authenticationError(AUTHENTICATION_ERROR_MSG));
+  try {
+    const decodeToken = validateToken(token);
+    return findUserByUsername(decodeToken.user)
+      .then(user => {
+        if (!user) return next(authenticationError(AUTHENTICATION_ERROR_MSG));
+        logger.info(`User ${user.username} authenticated successfully`);
+        req.session = { ...user.dataValues };
+        return next();
+      })
+      .catch(next);
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') return next(authenticationError(TOKEN_EXPIRED_ERROR_MSG));
+    return next(authenticationError(INVALID_TOKEN_ERROR_MSG));
+  }
+};
